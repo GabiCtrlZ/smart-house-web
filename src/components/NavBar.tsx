3b9d652d921559ccc1ebbd7cf9669b0d76c174a1
gabi balko <gab3009@gmail.com>
2021-10-26 03:16:14 +0300

    added return on agent page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@mui/styles'
 import { Theme } from '@mui/material'
 import IconButton from '@mui/material/IconButton'
 import { useHistory } from 'react-router'
 import homeImg from '../assets/icons/home.png'
 import menuImg from '../assets/icons/menu.png'
+import previousImg from '../assets/icons/previous.png'
 import Image from './generic/Image'
 import Menu from './Menu'
 
@@ -33,13 +34,27 @@ const NavBar = (): JSX.Element => {
   const classes = useStyles()
   const history = useHistory()
   const [open, setOpen] = useState(false)
+  const [back, setBack] = useState(false)
+
+  useEffect(() => {
+    return history.listen((location) => {
+      const id = window.location.pathname.split('/')[2]
+
+      console.log(`You changed the page to: ${location.pathname}`)
+      if (id) setBack(true)
+      else setBack(false)
+    })
+  }, [history])
 
   return (
     <>
       <div className={classes.root} >
-        <IconButton onClick={() => history.push('/home')}>
+        {!back && <IconButton onClick={() => history.push('/home')}>
           <Image src={homeImg} alt="home" size={30} />
-        </IconButton>
+        </IconButton>}
+        {back && <IconButton onClick={() => history.goBack()}>
+          <Image src={previousImg} alt="home" size={30} />
+        </IconButton>}
         <IconButton onClick={() => setOpen(true)}>
           <Image src={menuImg} alt="menu" size={30} />
         </IconButton>
